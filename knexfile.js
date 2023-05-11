4933996feade1a901c5875591d60fd8f28ef4a56
Akash Chauhan <akash.sc96@gmail.com>
2023-05-11 12:44:42 -0400

    Change the host value from localhost to 127.0.0.1

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 module.exports = {
   client: "mysql",
   connection: {
-    host: "localhost",
+    host: "127.0.0.1",
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
