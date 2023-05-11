81860feaf0b934d07ee45e1630615b7e553588a2
Akash Chauhan <akash.sc96@gmail.com>
2023-05-11 13:39:07 -0400

    Change the connection parameters and url for mysql

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+// const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+const urlDB = `mysql://root:EsZECG3dz8JNXNOAhV08@containers-us-west-136.railway.app:5471/railway`;
 
 // Connect database with Knex
 module.exports = {
   client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    charset: "utf8",
-  },
+  connection: urlDB,
+  // {
+  //   host: "127.0.0.1",
+  //   user: DB_USER,
+  //   password: DB_PASSWORD,
+  //   database: DB_NAME,
+  //   charset: "utf8",
+  // },
 };
