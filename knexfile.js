43e23dee23f5b70f17688fb44b5ca0379392e21c
Akash Chauhan <akash.sc96@gmail.com>
2023-05-11 15:23:58 -0400

    Fix connection settings to SQL databased created in Railway

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,10 @@
 require("dotenv").config();
+const mysql = require("mysql");
 
 // const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
-// const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+
+const connection = mysql.createConnection(urlDB);
 
 // Connect database with Knex
-module.exports = {
-  client: "mysql",
-  connection: {
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-  },
-};
+module.exports = connection;
