c1994429e55f4b9038e54b582683e6718892858c
Akash Chauhan <akash.sc96@gmail.com>
2023-05-11 14:58:11 -0400

    Change knex connection parameters to match railway database

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
 
 // const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+// const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 // Connect database with Knex
 module.exports = {
   client: "mysql",
-  connection: urlDB,
-  // {
-  //   host: "127.0.0.1",
-  //   user: DB_USER,
-  //   password: DB_PASSWORD,
-  //   database: DB_NAME,
-  //   charset: "utf8",
-  // },
+  connection: {
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+  },
 };
