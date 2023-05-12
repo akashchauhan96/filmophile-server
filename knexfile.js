8d178f3b6460e38ed1c3ec0475dd0aa9dca00f65
Akash Chauhan <akash.sc96@gmail.com>
2023-05-12 16:02:49 -0400

    Fix config on knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const mysql = require("mysql2");
-const knex = require("knex");
+// const mysql = require("mysql2");
+// const knex = require("knex");
 
 // const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.PORT}/${process.env.DB_NAME}`;
 
-const connection = mysql.createConnection(urlDB);
+// const connection = mysql.createConnection(urlDB);
 
-module.exports = knex({
-  client: "mysql2",
-  connection: connection,
-});
+module.exports = {
+  client: "mysql",
+  connection: urlDB,
+};
